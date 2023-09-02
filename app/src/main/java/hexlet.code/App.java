44f998989bf28918d68b9572f7470275d6d6a5a2
Konstantin Naumovich <konstantin000008@gmail.com>
2023-09-02 14:27:31 +0300

    new version Dockerfile

--- app/src/main/java/hexlet.code/App.java
+++ app/src/main/java/hexlet.code/App.java
@@ -5,7 +5,7 @@ import io.javalin.Javalin;
 public class App {
 
     private static int getPort() {
-        String port = System.getenv().getOrDefault("PORT", "8000");
+        String port = System.getenv().getOrDefault("PORT", "7070");
         return Integer.valueOf(port);
     }
     public static Javalin getApp() {
