5efd37dac9575866508198756a540b0e9856ca3b
Konstantin Naumovich <konstantin000008@gmail.com>
2023-09-02 10:16:34 +0300

    first version app for deploy with dockerfile

--- app/src/main/java/hexlet.code/App.java
+++ app/src/main/java/hexlet.code/App.java
@@ -1,9 +1,34 @@
 package hexlet.code;
 
+import io.javalin.Javalin;
+
 public class App {
 
-    public static void main(String[] args) {
+    private static int getPort() {
+        String port = System.getenv().getOrDefault("PORT", "8000");
+        return Integer.valueOf(port);
+    }
+    public static Javalin getApp() {
+        // Создаём приложение
+        Javalin app = Javalin.create(config -> {
+            // Включаем логгирование
+            config.plugins.enableDevLogging();
+        });
 
-        System.out.println("Test2");
+        // Добавляем маршруты в приложение
+        app.get("/", ctx -> ctx.result("Hello World"));
+
+        // Обработчик before запускается перед каждым запросом
+        // Устанавливаем атрибут ctx для запросов
+        app.before(ctx -> {
+            ctx.attribute("ctx", ctx);
+        });
+
+        return app;
+    }
+
+    public static void main(String[] args) {
+        Javalin app = getApp();
+        app.start(getPort());
     }
 }
