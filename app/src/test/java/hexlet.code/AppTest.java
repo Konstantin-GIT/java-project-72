d037ff5d1b78cbd710cb1de4b2a598dd772f019c
Konstantin Naumovich <konstantin000008@gmail.com>
2023-09-14 11:25:30 +0300

    add first test

---
+++ app/src/test/java/hexlet.code/AppTest.java
@@ -0,0 +1,16 @@
+import hexlet.code.App;
+import org.junit.jupiter.api.Test;
+
+
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class AppTest {
+
+    @Test
+    public void assertTest() throws IOException {
+
+        assertEquals(1, 1);
+    }
+    }
