076ec416682eda1fbd5f930acb805223f43382b6
Konstantin Naumovich <konstantin000008@gmail.com>
2023-09-14 16:14:17 +0300

    added variable JDBC_DATABASE_URL

--- app/src/main/java/hexlet/code/repository/BaseRepository.java
+++ app/src/main/java/hexlet/code/repository/BaseRepository.java
@@ -7,9 +7,28 @@ import com.zaxxer.hikari.HikariDataSource;
  * Represents the base repository class.
  */
 public class BaseRepository {
-    private static final String JDBC_URL = "jdbc:h2:mem:hikariDB";
-    private static final String USERNAME = "username";
-    private static final String PASSWORD = "password";
+
+    private static int getPort() {
+        String port = System.getenv().getOrDefault("PORT", "7070");
+        return Integer.valueOf(port);
+    }
+    private static final String JDBC_URL_H2 = "jdbc:h2:mem:hikariDB";
+
+    private static final String JDBC_URL = getJdbcDatabaseUrl();
+
+    public static String getJdbcDatabaseUrl() {
+        // Получаем значение переменной окружения JDBC_DATABASE_URL
+        String jdbcUrl = System.getenv("JDBC_DATABASE_URL");
+
+        // Если переменная окружения не установлена, устанавливаем значение по умолчанию
+        if (jdbcUrl == null || jdbcUrl.isEmpty()) {
+            jdbcUrl = JDBC_URL_H2; // Значение по умолчанию
+        }
+
+        return jdbcUrl;
+    }
+    private static final String USERNAME = "konstantin";
+    private static final String PASSWORD = "QYZgokSmgU0jOVY1hjnizbe7xoMeKDih";
     private static HikariDataSource dataSource;
 
     static {
