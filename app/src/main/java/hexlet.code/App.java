ff8f665c6edaea4b95991cd9f22109df04be03d6
Konstantin Naumovich <konstantin000008@gmail.com>
2023-09-15 13:36:05 +0300

    added JTE

--- app/src/main/java/hexlet.code/App.java
+++ app/src/main/java/hexlet.code/App.java
@@ -1,10 +1,16 @@
 package hexlet.code;
 
+import gg.jte.resolve.ResourceCodeResolver;
 import hexlet.code.model.Url;
 import hexlet.code.repository.UrlsRepository;
 import io.javalin.Javalin;
+import gg.jte.ContentType;
+import gg.jte.TemplateEngine;
+import io.javalin.rendering.template.JavalinJte;
 
 import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class App {
 
@@ -18,9 +24,13 @@ public class App {
             // Включаем логгирование
             config.plugins.enableDevLogging();
         });
+        JavalinJte.init(createTemplateEngine());
 
         // Добавляем маршруты в приложение
         app.get("/", ctx -> ctx.result("Hello World"));
+
+
+        app.get("/render", ctx -> ctx.render("Welcome.jte", Map.of("title", "TEST!!!!!!!!")));
 
         // Обработчик before запускается перед каждым запросом
         // Устанавливаем атрибут ctx для запросов
@@ -30,6 +40,14 @@ public class App {
 
         return app;
     }
+
+    private static TemplateEngine createTemplateEngine() {
+        ClassLoader classLoader = App.class.getClassLoader();
+        ResourceCodeResolver codeResolver = new ResourceCodeResolver("templates", classLoader);
+        TemplateEngine templateEngine = TemplateEngine.create(codeResolver, ContentType.Html);
+        return templateEngine;
+    }
+
 
     public static void main(String[] args) throws SQLException {
         Javalin app = getApp();
